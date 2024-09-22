4fbcb7be67303ccd66b51a57f3747613eb5acf68
Kaushik raj <algorithmunloack@gmail.com>
2024-09-22 13:14:57 +0530

    The login page development with the react-native

--- App.jsx
+++ App.jsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import {Text,View,TextInput, StyleSheet} from 'react-native';
-
-const Red = () => {
+import React , {useState}from 'react';
+import {Button, Text, View} from 'react-native';
+const App  = () => {
+  const [count, set] = useState(0);
   return (
-    <View style={styles.container}>
-    <Text style={styles.test1}> This is ok</Text>
-    <Text style={styles.test1}> This is </Text>
-    
-
+    <View> 
+      <Text> Count: {count}</Text>
+      <Button title = "OK" onPress={() => set(count + 1)} />
     </View>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignitems: 'center',
-  },
-  test1: {
-    alignContent:'center',
-    backgroundColor: 'green',
-    fontWeight: 'bold',
-    fontSize:20,
-    padding:10,
-}
-});
-export default Red;
+export default App;
